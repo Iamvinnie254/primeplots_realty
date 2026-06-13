4eee1471727e1e63091a5edf28391411a56abade
stephen <vincentstephen489@gmail.com>
2026-06-13 22:33:52 +0300

    folder structure setup and react router for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout/Layout";
+import Home from "./pages/home/Home";
+import Page_404 from "./pages/404/Page_404";
+import About from "./pages/about/About";
+import Blog from "./pages/blog/Blog";
+import Contact from "./pages/contact/Contact";
+import FAQ from "./pages/faq/FAQ";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="blog" element={<Blog />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="faq" element={<FAQ />} />
+            <Route path="*" element={<Page_404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
